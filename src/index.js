d618a9feed29aebe509b7c5c2581e337eb91e9d6
Bulin <st234@outlook.co.th>
2017-06-23 21:04:19 +0700

    init firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyAi4LDAXirt_xnXLu5KnypbcVO9rRr_Ju0",
+    authDomain: "hackaton-2d427.firebaseapp.com",
+    databaseURL: "https://hackaton-2d427.firebaseio.com",
+    projectId: "hackaton-2d427",
+    storageBucket: "hackaton-2d427.appspot.com",
+    messagingSenderId: "621420486419"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
