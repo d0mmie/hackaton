33fe2ee36225811ed0532952282880ec615dc513
Bulin <st234@outlook.co.th>
2017-06-23 21:39:20 +0700

    test notification and geo location

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import {RaisedButton} from 'material-ui'
+import ReactTapEvent from 'react-tap-event-plugin'
+
+ReactTapEvent()
 
 class App extends Component {
+  state = {
+    latitude:'',
+    longtitude:''
+  }
+  
   render() {
+   const getLocation= () =>{
+    Notification.requestPermission().then((res) => console.log(res))
+    navigator.geolocation.getCurrentPosition((geo)=>this.setState({latitude:geo.coords.latitude,longtitude:geo.coords.longitude}))
+  }
     return (
       <div className="App">
-        It's Work
+        <RaisedButton label="Get Geo" onTouchTap={getLocation}  />
+        {JSON.stringify(this.state)}
       </div>
     );
   }
