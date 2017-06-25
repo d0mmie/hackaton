fe125a5f92ba718a0ea47df2aff5f00d8745fa3c
Bulin <st234@outlook.co.th>
2017-06-25 14:33:20 +0700

    cheer list

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import ReduxCon from '../redux/redux-connect'
-import {Card,CardHeader,CardText,TextField,CircularProgress} from 'material-ui'
+import {Card,CardHeader,CardText,TextField,CircularProgress,Paper} from 'material-ui'
 import {Link} from 'react-router-dom'
+import firebase from 'firebase'
 
 class HomePage extends Component {
+    state = {
+        team1:'',
+        team2:'',
+        descript:'',
+        allPoll:null,
+        loading:true
+    }
     componentDidMount(){
+        firebase.database().ref().child('allPoll').on('value',(snap)=>{
+            this.setState({allPoll:snap.val(),loading:false})
+        })
         this.props.SetSelected(0)
     }
     render() {
         return (
             <div>
-                {this.props.store.user?(this.props.store.isLogin === true?<Card style={{margin:20}} >
-                    <CardHeader avatar={this.props.store.user.photoURL} actAsExpander showExpandableButton title="Submit your Cheer" subtitle={this.props.store.user?this.props.store.user.displayName:'Loading...'} />
+                {this.props.store.user?(this.props.store.isLogin === true?<Card >
+                    <CardHeader avatar={this.props.store.user.photoURL} actAsExpander showExpandableButton title="Submit your Cheer" subtitle="Tap to expand" />
                     <CardText expandable>
-                        <p><TextField multiLine fullWidth floatingLabelText="Your descript" /></p>
+                        <p><TextField value={this.state.descript} onChange={(e)=>this.setState({descript:e.target.value})} multiLine fullWidth floatingLabelText="Your descript" /></p>
+                        <p></p>
                     </CardText>
                 </Card>: <Card style={{margin:20}} >
                         <CardHeader title="You're not Sign in" />
@@ -21,6 +33,12 @@ class HomePage extends Component {
                             <p>Clink <Link to="/profile">Here</Link></p>
                         </CardText>
                     </Card>):<div style={{textAlign:'center',padding:10}} ><CircularProgress /></div>}
+                    {this.state.loading === false?(this.state.allPoll === null?<Paper style={{textAlign:'center',padding:20,marginTop:20,color:'#aaaaaa'}} zDepth={1} >Nothing Poll</Paper>:Object.keys(this.state.allPoll).map((key)=>
+                        <Paper style={{padding:10,marginTop:20,cursor:'pointer'}} onTouchTap={()=>this.props.history.push(`/m/${key}`)}>
+                         <p style={{fontWeight:'bold'}} >{this.state.allPoll[key].team1} vs {this.state.allPoll[key].team2}</p>
+                         <p>{this.state.allPoll[key].descript}</p>
+                        </Paper>
+                    )):null}
             </div>
         );
     }
